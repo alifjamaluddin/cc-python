a316e58c3025ebc801c3ba06c3738aef86d0ddb1
Alif Jamaluddin <alif.jamaluddin@tmrnd.com.my>
2023-11-05 21:40:56 +0800

    Added -m option and read from stdin

--- wc/src/app.py
+++ wc/src/app.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-
+import sys
+import subprocess
 parser = argparse.ArgumentParser(
                     prog='ccwc',
                     description="""
@@ -14,9 +15,9 @@ If more than one input file is specified, a line of cumulative counts for all th
 parser.add_argument('-c', action='store_true', help="The number of bytes in each input file is written to the standard output.")      
 parser.add_argument('-l', action='store_true', help="The number of lines in each input file is written to the standard output.")      
 parser.add_argument('-w', action='store_true', help="The number of words in each input file is written to the standard output.")      
-# parser.add_argument('-m', action='store_true', help="The number of characters in each input file is written to the standard output.")      
+parser.add_argument('-m', action='store_true', help="The number of characters in each input file is written to the standard output.")      
 
-parser.add_argument('file', metavar="File", help='file path')
+parser.add_argument('file', nargs='?',metavar="File", help='file path')
 
 args = parser.parse_args()
 
@@ -24,28 +25,28 @@ filepath = args.file
 enableCountByte = args.c
 enableCountLine = args.l
 enableCountWord = args.w
+enableCountChar = args.m
 
-if not os.path.isfile(filepath):
-    print("Error: No such file")
-    exit()
 
-f = open(os.path.abspath(filepath), 'r')
-content = f.read()
-f.close()
-print(args)
+if filepath != None:
+    if not os.path.isfile(filepath):
+        print("Error: No such file")
+        exit()
 
-if not enableCountWord and not enableCountByte and not enableCountLine:
-    print("{lineCount} {wordCount} {byteCount} {filepath}".format(  wordCount= len(content.split()) if enableCountWord else "", \
-                                                                    lineCount=len(content.split('\n')) if enableCountLine else "", \
-                                                                    byteCount=len(content) if enableCountByte else "", \
-                                                                    filepath=filepath))
+    f = open(os.path.abspath(filepath), 'r')
+    content = f.read()
+    f.close()
+else:
+    content = sys.stdin.read() # get input from stdin
+
+if (not enableCountWord) and (not enableCountByte) and (not enableCountLine) and (not enableCountChar):
+    print("{lineCount} {wordCount} {byteCount} {filepath}".format( lineCount=len(content.split('\n')), wordCount=len(content.split()), byteCount=len(content), filepath=filepath if filepath != None else ""))
+
 elif enableCountLine:
-    print("{lineCount} {filepath}".format(lineCount=len(content.split('\n')) if enableCountLine else "", filepath=filepath))
+    print("{lineCount} {filepath}".format(lineCount=len(content.split('\n')) if enableCountLine else "", filepath=filepath if filepath != None else ""))
 elif enableCountByte:
-    print("{byteCount} {filepath}".format(byteCount=len(content) if enableCountByte else "", filepath=filepath))
+    print("{byteCount} {filepath}".format(byteCount=len(content), filepath=filepath if filepath != None else ""))
 elif enableCountWord:
-    print("{wordCount} {filepath}".format(wordCount= len(content.split()) if enableCountWord else "", filepath=filepath))
-
-# todo:
-# 1. -m option, function to count character
-# 2. get input from standard input
+    print("{wordCount} {filepath}".format(wordCount= len(content.split()), filepath=filepath if filepath != None else ""))
+elif enableCountChar:
+    print("{charCount} {filepath}".format(charCount= len(content.decode("utf-8", "ignore")), filepath=filepath if filepath != None else ""))
